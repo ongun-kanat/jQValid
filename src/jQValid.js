3a5bbdfabf3cb38d976acca5f76b195ecc606b22
Ongun Kanat <ongun.kanat@gmail.com>
2014-06-05 04:08:37 +0300

    jQuery plugin skeleton created

---
+++ src/jQValid.js
@@ -0,0 +1,14 @@
+/*
+ * jQValid a jQuery Validation Plugin by Ongun Kanat, <ongun.kanat@gmail.com>
+ * 
+ * Last Edited: 2014-06-05
+ */
+
+(function( $ ) {
+    $.fn.jQValid = function ( arguments )
+    {
+        return this.each(function (){
+            
+        });
+    }
+} (jQuery));
